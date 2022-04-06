7e33bdfa61739867335f4736e3ccf3fc167b32d2
Dmytro <dmytro.vasylenko@pics.io>
2022-04-06 21:55:27 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function App() {
+   return (
+      <>
+         <Header />
+         <div>
+            <div className="App">
+               <div className="test">Main test</div>
+            </div>
+         </div>
+         <Footer />
+      </>
+   );
+}
+
+export default App;
