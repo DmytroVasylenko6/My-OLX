6c917793cfc38629f3c86e29d2ecae6661d86bb8
Slavik <babych.slavik2016@gmail.com>
2022-04-29 20:55:59 +0300

    header, main, footer (finish)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
+
+
 
 
 function App() {
    return (
       <>
          <Header />
-         <div>
-            <div className="App">
-               <div className="test">Main test</div>
-            </div>
-         </div>
+         
+         <Main />
+        
          <Footer />
+         
       </>
    );
 }
